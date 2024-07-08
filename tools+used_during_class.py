39d3e9612cae09128b1d73851368eec9c9111682
blair-developer <blairdeveloper17@gmail.com>
2024-07-08 20:54:26 +0300

    first commit

---
+++ tools+used_during_class.py
@@ -0,0 +1,7 @@
+Download DB BROWSER:  https://sqlitebrowser.org/dl/ - Relational database management system.
+     or
+follow this url https://github.com/blair-developer/GDA2_CLASS and download the tool.
+
+
+For the sql commands statements find at sql_queries.py file
+
